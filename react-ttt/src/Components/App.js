f3faa613922d7bd723b5598d289858865e3b1694
Josiah Aubin <josiah.aubin@intimetec.com>
2021-06-15 16:12:28 -0600

    Updated the file structure

---
+++ react-ttt/src/Components/App.js
@@ -0,0 +1,13 @@
+import '../Stylings/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h1>Test</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;

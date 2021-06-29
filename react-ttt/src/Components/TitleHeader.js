dd538dc90fff5827ab484a939b891338a59a69e5
josiahaubin <josiahaubin@gmail.com>
2021-06-29 15:05:47 -0600

    React Logo Spinning

--- react-ttt/src/Components/TitleHeader.js
+++ react-ttt/src/Components/TitleHeader.js
@@ -4,7 +4,7 @@ import logo from '../logo.svg';
 function TitleHeader() {
     return(
         <div className="Header-titlebox">
-            <h1 className="Game-title"><img src={logo} alt="React Logo" />React Tic-Tac-Toe</h1>
+            <h1 className="Game-title"><img className='React-img' src={logo} alt="React Logo" />React Tic-Tac-Toe</h1>
             <p>Welcome to React Tic-Tac-Toe! Press play now to get started!</p>
         </div>
     );
